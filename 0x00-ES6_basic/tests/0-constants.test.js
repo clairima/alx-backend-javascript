04109d5330618b50736c5654573a73a68efd2da5
clairima <mertahrima@gmail.com>
2024-01-02 21:11:24 +0100

    eslint

--- 0x00-ES6_basic/tests/0-constants.test.js
+++ 0x00-ES6_basic/tests/0-constants.test.js
@@ -2,6 +2,7 @@ import { taskFirst, taskNext } from '../0-constants';
 
 describe('taskFirst function', () => {
   it('should return a string containing "const"', () => {
+    expect.assertions(1);
     const result = taskFirst();
     expect(result).toContain('const');
   });
@@ -9,6 +10,7 @@ describe('taskFirst function', () => {
 
 describe('taskNext function', () => {
   it('should return a string containing "let" and "is okay"', () => {
+    expect.assertions(2);
     const result = taskNext();
     expect(result).toContain('let');
     expect(result).toContain('is okay');
