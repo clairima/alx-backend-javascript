a50adb406c96a079a8675e29f16b6273c092fa71
clairima <mertahrima@gmail.com>
2024-01-03 21:35:34 +0100

    task 2

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const responseData = { status: 200, data: 'Some data from API' };
-      resolve(responseData);
-    }, 1000);
-  });
+  return (new Promise(() => {}));
 }
